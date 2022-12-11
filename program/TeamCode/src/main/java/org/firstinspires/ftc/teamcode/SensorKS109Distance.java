26cb7787a258e15df9e68e427c3e2ceb200c4730
sunpan <sunpan@gmail.com>
2022-12-10 23:37:52 -0800

    Update SensorKS109Distance.java
    Add OpMode SensorKS109Distance

--- program/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorKS109Distance.java
+++ program/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorKS109Distance.java
@@ -42,17 +42,16 @@ import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@TeleOp(name = "Sensor: MR ODS", group = "Sensor")
-@Disabled
+@TeleOp(name = "Sensor: KS 109", group = "Sensor")
 public class SensorKS109Distance extends LinearOpMode {
 
-  OpticalDistanceSensor odsSensor;  // Hardware Device Object
+  //OpticalDistanceSensor odsSensor;  // Hardware Device Object
 
   @Override
   public void runOpMode() {
 
     // get a reference to our Light Sensor object.
-    odsSensor = hardwareMap.get(OpticalDistanceSensor.class, "sensor_ods");
+  //  odsSensor = hardwareMap.get(OpticalDistanceSensor.class, "sensor_ods");
 
     // wait for the start button to be pressed.
     waitForStart();
@@ -62,8 +61,8 @@ public class SensorKS109Distance extends LinearOpMode {
     while (opModeIsActive()) {
 
       // send the info back to driver station using telemetry function.
-      telemetry.addData("Raw",    odsSensor.getRawLightDetected());
-      telemetry.addData("Normal", odsSensor.getLightDetected());
+      telemetry.addData("Raw",    0);
+      telemetry.addData("Normal", 0);
 
       telemetry.update();
     }
